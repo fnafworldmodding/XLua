522d91f2fa982f2009d87e18cc1d1def688767c0
programminglaboratorys <programming.laboratorys@gmail.com>
2025-06-22 20:21:20 +0300

    xlua object pointer being a global variable

---
+++ XLua/Globals.cpp
@@ -0,0 +1,5 @@
+#include "Globals.h"
+
+bool preInit = false;
+XLuaObject* globalXLua = nullptr;
+
---
+++ XLua/Globals.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef XLUAGLOBALS_H_
+#define XLUAGLOBALS_H_
+#include <atomic>
+#include "Common.h"
+
+extern bool preInit;
+extern XLuaObject* globalXLua;
+//#define globalXLua GETGLOBALXLUA()
+
+#endif // !XLUAGLOBALS_H_
